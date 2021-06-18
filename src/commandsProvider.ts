39fa1f1d7ce6b7dfe965f2d64d78fd28250b1da3
karpad2 <karpad2@gmail.com>
2021-06-18 13:38:02 +0200

    fixek

--- src/commandsProvider.ts
+++ src/commandsProvider.ts
@@ -104,7 +104,7 @@ export function rebolRunInGuiConsole(fileUri?: vscode.Uri) {
 	let filePath = getFileName(fileUri);
 	if (filePath === '') {return;}
 	let ext = path.parse(filePath).ext.toLowerCase();
-	if (ext !== ".rebol") {
+	if (ext !== ".sc") {
 		vscode.window.showErrorMessage("don't support " + ext + " file");
 		return;
 	}
